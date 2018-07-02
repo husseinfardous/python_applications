56c35af00e3df68b77e20bc5b63a4ac8575407da
Hussein Fardous <hf2334@columbia.edu>
2018-07-01 23:07:06 -0400

    Interactive Dictionary: Accounted for Words that don't Exist in Dictionary and for Different Letter Cases

--- interactive_dictionary/app.py
+++ interactive_dictionary/app.py
@@ -1,11 +1,18 @@
 import json
 
-file = open("data.json", "rb")
-data = json.load(file)
+def definition(dictionary, word):
 
-def definition(word):
-    return data[word]
+    word = word.lower()
+
+    if word in dictionary:
+        return dictionary[word]
+    else:
+        return "Sorry: The word doesn't exist in the dictionary. Please try another word."
 
 if __name__ == "__main__":
+
+    file = open("data.json", "rb")
+    data = json.load(file)
+
     word = input("Please enter the word that you seek the definition of: ")
-    print(definition(word))
+    print(definition(data, word))
