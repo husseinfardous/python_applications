11810f7c848435d76e8d386dd97fc1c965b11a25
Hussein Fardous <hf2334@columbia.edu>
2018-07-02 00:21:38 -0400

    Interactive Dictionary: Added Similar Word Suggestion Functionality

--- interactive_dictionary/app.py
+++ interactive_dictionary/app.py
@@ -1,4 +1,8 @@
+from difflib import get_close_matches
 import json
+
+def similar_word(dictionary, word):
+    return get_close_matches(word, dictionary.keys())
 
 def definition(dictionary, word):
 
@@ -6,6 +10,21 @@ def definition(dictionary, word):
 
     if word in dictionary:
         return dictionary[word]
+
+    elif len(similar_word(dictionary, word)) > 0:
+
+        similarWord = similar_word(dictionary, word)[0]
+        response = input("Did you mean %s? Please enter 'yes' or 'no': " % (similarWord))
+
+        if response == "yes":
+            return dictionary[similarWord]
+
+        elif response == "no":
+            return "Sorry: The word doesn't exist in the dictionary. Please try another word."
+
+        else:
+            return "Sorry: Invalid Response"
+
     else:
         return "Sorry: The word doesn't exist in the dictionary. Please try another word."
 
