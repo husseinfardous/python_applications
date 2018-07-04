7c544f175f4ff7fbfc7e88ae9c699735f016588f
Hussein Fardous <hf2334@columbia.edu>
2018-07-03 23:14:57 -0400

    Web Map: Added Point Markers at Locations from CSV File

--- web_map/app.py
+++ web_map/app.py
@@ -1,4 +1,17 @@
 import folium
+import pandas
 
-map = folium.Map(location = [41, -74], zoom_start = 15)
+data = pandas.read_csv("volcanoes_usa.csv")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
+map = folium.Map(location = [40.758936, -73.985099], zoom_start = 10, tiles = "Mapbox Bright")
+
+featureGroup = folium.FeatureGroup(name = "Map")
+
+for lt, ln in zip(lat, lon):
+    featureGroup.add_child(folium.Marker(location = [lt, ln], popup = "Marker", icon = folium.Icon(color = "red")))
+
+map.add_child(featureGroup)
+
 map.save("map.html")
