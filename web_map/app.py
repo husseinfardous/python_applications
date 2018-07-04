d8f6b774c9712e98f9cfac48719f771e31d878e7
Hussein Fardous <hf2334@columbia.edu>
2018-07-03 23:42:48 -0400

    Web Map: Created Popup Windows for Point Markers with CSV File Elevation Data

--- web_map/app.py
+++ web_map/app.py
@@ -4,13 +4,14 @@ import pandas
 data = pandas.read_csv("volcanoes_usa.csv")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location = [40.758936, -73.985099], zoom_start = 10, tiles = "Mapbox Bright")
 
 featureGroup = folium.FeatureGroup(name = "Map")
 
-for lt, ln in zip(lat, lon):
-    featureGroup.add_child(folium.Marker(location = [lt, ln], popup = "Marker", icon = folium.Icon(color = "red")))
+for lt, ln, el in zip(lat, lon, elev):
+    featureGroup.add_child(folium.Marker(location = [lt, ln], popup = "Elevation: " + str(el) + "m", icon = folium.Icon(color = "red")))
 
 map.add_child(featureGroup)
 
