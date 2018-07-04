58df25dfd9f8c2d25bc47975dcab7e5d67f44bd1
Hussein Fardous <hf2334@columbia.edu>
2018-07-04 10:39:57 -0400

    Web Map: Added GeoJSON Polygon Layer

--- web_map/app.py
+++ web_map/app.py
@@ -14,13 +14,21 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
-map = folium.Map(location = [40.758936, -73.985099], zoom_start = 10, tiles = "Mapbox Bright")
+file = open("countries.json", "r", encoding = "utf-8-sig")
+
+map = folium.Map(location = [40.758936, -73.985099], zoom_start = 5, tiles = "Mapbox Bright")
 
 featureGroup = folium.FeatureGroup(name = "Map")
 
 for lt, ln, el in zip(lat, lon, elev):
     featureGroup.add_child(folium.CircleMarker(location = [lt, ln], radius = 6, popup = "Elevation: " + str(el) + "m", fill_color = choose_color(el), color = "grey", fill = True, fill_opacity = 0.85))
 
-map.add_child(featureGroup)
+lbd = lambda feature: {"fillColor": "green" if feature["properties"]["POP2005"] < 10000000
+        else "orange" if 10000000 <= feature["properties"]["POP2005"] <= 20000000
+        else "red"}
+featureGroup.add_child(folium.GeoJson(data = file.read(), style_function = lbd))
 
+map.add_child(featureGroup)
 map.save("map.html")
+
+file.close()
