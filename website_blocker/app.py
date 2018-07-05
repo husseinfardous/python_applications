919ebe45cce3a29c0e861a37e830b84a85276219
Hussein Fardous <hf2334@columbia.edu>
2018-07-04 20:09:58 -0400

    Website Blocker: Enabled Python Script to Add Websites to Block and to Redirect to in 'hosts' File

--- website_blocker/app.py
+++ website_blocker/app.py
@@ -1,8 +1,16 @@
-from urllib.parse import urlparse
 import argparse
-import requests
 
-def block(os, websites):
+import requests
+from urllib.parse import urlparse
+
+import time
+from datetime import datetime as dt
+
+import sys
+
+import socket
+
+def block(os, redirect_site, websites, start_time, end_time):
 
     hostsFilePath = "/etc/hosts"
     if os == "Windows":
@@ -10,10 +18,16 @@ def block(os, websites):
     else:
         if os != "macOS" and os != "Linux":
             print("ERROR: No Valid Value for the --os Flag was Supplied!")
-            print("USAGE: python app.py --os <macOS, Linux, or Windows>")
+            print("USAGE: python app.py --os <macOS, Linux, or Windows> --redirect_site <URL> --websites <URL-1> <Optional: URL-2> <...> --start_time <Hour: 0 - 23> <Minute: 0 - 59> --end_time <Hour: 0 - 23> <Minute: 0 - 59>")
             exit(1)
 
     try:
+
+        request = requests.get(redirect_site)
+
+        urlParser = urlparse(redirect_site)
+        hostname = urlParser.netloc
+        redirect_ip = socket.gethostbyname(hostname)
 
         websites_block_list = []
 
@@ -29,18 +43,38 @@ def block(os, websites):
             else:
                 websites_block_list.append("www." + hostname)
 
+        while True:
+
+            if dt(dt.now().year, dt.now().month, dt.now().day, int(start_time[0]), int(start_time[1])) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, int(end_time[0]), int(end_time[1])):
+
+                print("Work Hours...")
+                with open("hosts", "r+") as file:
+                    content = file.read()
+                    for website in websites_block_list:
+                        if website not in content:
+                            file.write(redirect_ip + " " + website + "\n")
+
+            else:
+                print("Fun Hours...")
+
+            time.sleep(5)
+            sys.stdout.flush()
+
     except requests.exceptions.MissingSchema:
         print("ERROR: Invalid URL! No Schema Supplied!")
     except requests.exceptions.ConnectionError:
         print("ERROR: Website doesn't Exist!")
-
-    redirect_ip = "127.0.0.1"
+    except ValueError:
+        print("ERROR: Start Time and End Time: Hours must be between 0 and 23, Minutes must be between 0 and 59!")
 
 if __name__ == "__main__":
 
     argParser = argparse.ArgumentParser()
     argParser.add_argument("--os", required = True, help = "What's your Operating System: macOS, Linux, or Windows?")
+    argParser.add_argument("--redirect_site", required = True, help = "Enter the URL you wish to Redirect to.")
     argParser.add_argument("--websites", nargs='+', required = True, help = "List the URLs of the Websites you wish to Block.")
+    argParser.add_argument("--start_time", nargs='+', required = True, help = "Time (Hour (Format: 0 - 23) and Minute (0 - 59)) to Initiate Website Blocking.")
+    argParser.add_argument("--end_time", nargs='+', required = True, help = "TIme (Hour (Format: 0 - 23) and Minute (0 - 59)) to Terminate Website Blocking.")
     args = argParser.parse_args()
 
-    block(args.os, args.websites)
+    block(args.os, args.redirect_site, args.websites, args.start_time, args.end_time)
