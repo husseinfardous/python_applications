8f3b6635acb4c162bedb29d54773e00483f32350
Hussein Fardous <hf2334@columbia.edu>
2018-07-19 00:14:37 -0400

    Web Scraper: Extracted Real Estate Property Data

--- web_scraper/app.py
+++ web_scraper/app.py
@@ -20,10 +20,60 @@ from bs4 import BeautifulSoup
 
 
 
-# Load First Page
+# Extract Data for all of the Real Estate Properties on the First Page
 
 # GET Request to the Server to Receive the HTML Document
 first_page = requests.get("https://pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/").content
 
 # Get BeautifulSoup object that represents the HTML Document as a Nested Data Structure for Simple Navigation 
 first_page_soup = BeautifulSoup(first_page, "html.parser")
+
+# Extract all of the Real Estate Properties in Full and Collect in a List
+# Each Real Estate Property belongs to a "div" Tag with Class "propertyRow"
+all_properties = first_page_soup.find_all("div", {"class": "propertyRow"})
+
+# Extract Data for Each Real Estate Property
+for single_property in all_properties:
+
+    # Extract the Price
+    price = single_property.find("h4", {"class": "propPrice"}).text.replace("\n", "").replace(" ", "")
+    print(price)
+
+    # Extract the Address
+    address_first_half = single_property.find_all("span", {"class": "propAddressCollapse"})[0].text
+    print(address_first_half)
+    address_second_half = single_property.find_all("span", {"class": "propAddressCollapse"})[1].text
+    print(address_second_half)
+
+    # Extract the Number of Beds if it Exists
+    beds = single_property.find("span", {"class": "infoBed"})
+    if beds != None:
+        beds = beds.find("b").text
+    print(beds)
+
+    # Extract the Area in Square Feet if it Exists
+    area = single_property.find("span", {"class": "infoSqFt"})
+    if area != None:
+        area = area.find("b").text
+    print(area)
+
+    # Extract the Number of Full Baths if it Exists
+    full_baths = single_property.find("span", {"class": "infoValueFullBath"})
+    if full_baths != None:
+        full_baths = full_baths.find("b").text
+    print(full_baths)
+
+    # Extract the Number of Half Baths if it Exists
+    half_baths = single_property.find("span", {"class": "infoValueHalfBath"})
+    if half_baths != None:
+        half_baths = half_baths.find("b").text
+    print(half_baths)
+
+    # Extract the Lot Size if it Exists
+    # The Lot Size is tricky because there are multiple "span" Tags with Class "featureGroup" that contain features other than Lot Size
+    for column_group in single_property.find_all("div", {"class": "columnGroup"}):
+        for feature_group, feature_name in zip(column_group.find_all("span", {"class": "featureGroup"}), column_group.find_all("span", {"class": "featureName"})):
+            if "Lot Size" in feature_group.text:
+                print(feature_name.text)
+
+    print("")
