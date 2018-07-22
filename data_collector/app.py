cf42e3e5ba5394b1e6a0d7e4da848495d7a625c5
Hussein Fardous <hf2334@columbia.edu>
2018-07-22 14:24:27 -0400

    Data Collector: Backend - Created the PostgreSQL Database Model

--- data_collector/app.py
+++ data_collector/app.py
@@ -4,13 +4,38 @@
 
 # Import Modules
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 
 
-# Configure Flask
+# Configure Flask and Connect to PostgreSQL Database "data_collector"
 
 # Create an Instance of Flask Class for the Web Application
 app = Flask(__name__)
+
+# Connect Flask to PostgreSQL Database "data_collector"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/data_collector"
+
+# Create SQLAlchemy object
+# SQLAlchemy facilitates Interaction between Flask and PostgreSQL
+db = SQLAlchemy(app)
+
+# Create Class that Represents Blueprint of Data to Collect
+# Inherit from SQLAlchemy Model Class
+class Data(db.Model):
+
+    # Create a Table called "data"
+    # Add Three Columns to the Table: id, email, and age
+    # The Three Columns are the Data Class' Properties
+    __tablename__ = "data"
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(120), unique = True)
+    age = db.Column(db.Integer)
+
+    # Initialize Properties
+    def __init__(self, email, age):
+        self.email = email
+        self.age = age
 
 # Display User Submission Webpage
 @app.route("/")
