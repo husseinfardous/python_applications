173e885135089cf96109f3724f0ed789f268df3b
Hussein Fardous <hf2334@columbia.edu>
2018-07-28 10:51:31 -0400

    Website Blocker: Explained Code in Comments

--- website_blocker/app.py
+++ website_blocker/app.py
@@ -1,17 +1,28 @@
-import argparse
+# Website Blocker Application
 
+# Blocks Given Website(s) from Given Start Time until Given End Time
+# Gives Users Flexibility because it Depends on User Input
+
+# Import Modules
+import argparse
 import requests
 from urllib.parse import urlparse
-
 import time
 from datetime import datetime as dt
-
 import sys
-
 import socket
 
+
+
+# Block Given Website(s) from Given Start Time until Given End Time
+# Handle Invalid User Input
 def block(os, redirect_site, websites, start_time, end_time):
 
+
+
+    # Path to "Hosts" File (Enables Website Blocking)
+    # Path differs between macOS/Linux and Windows
+    # User Expected to Enter Current Operating System
     hostsFilePath = "/etc/hosts"
     if os == "Windows":
         hostsFilePath = r"C:\Windows\System32\drivers\etc\hosts"
@@ -21,14 +32,27 @@ def block(os, redirect_site, websites, start_time, end_time):
             print("USAGE: python app.py --os <macOS, Linux, or Windows> --redirect_site <URL> --websites <URL-1> <Optional: URL-2> <...> --start_time <Hour: 0 - 23> <Minute: 0 - 59> --end_time <Hour: 0 - 23> <Minute: 0 - 59>")
             exit(1)
 
+
+
     try:
 
+
+
+        # Check if Given Website to Redirect to when Accessing Target Website is Valid
+        # Exception ConnectionError Generated if not Valid
         request = requests.get(redirect_site)
 
+        # Convert Given Website to Redirect to when Accessing Target Website to an IP Address
         urlParser = urlparse(redirect_site)
         hostname = urlParser.netloc
         redirect_ip = socket.gethostbyname(hostname)
 
+
+
+        # Collect Given Website(s) to Block in a List
+        # Store Two Versions for Each Target Website:
+        # First Version without "www."
+        # Second Version with "www."
         websites_block_list = []
 
         for website in websites:
@@ -43,10 +67,15 @@ def block(os, redirect_site, websites, start_time, end_time):
             else:
                 websites_block_list.append("www." + hostname)
 
+
+
+        # Infinite Loop
         while True:
 
+            # If the Current Time is between Given Start and End Times
             if dt(dt.now().year, dt.now().month, dt.now().day, int(start_time[0]), int(start_time[1])) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, int(end_time[0]), int(end_time[1])):
 
+                # Add Target Website and IP Address to Redirect to in "Hosts" File if not Already there
                 print("Work Hours...")
                 with open(hostsFilePath, "r+") as file:
                     content = file.read()
@@ -54,8 +83,10 @@ def block(os, redirect_site, websites, start_time, end_time):
                         if website not in content:
                             file.write(redirect_ip + " " + website + "\n")
 
+            # The Current Time is not between Given Start and End Times
             else:
 
+                # Remove Target Website and IP Address to Redirect to from "Hosts" File if there
                 print("Fun Hours...")
                 with open(hostsFilePath, "r+") as file:
                     content = file.readlines()
@@ -65,9 +96,15 @@ def block(os, redirect_site, websites, start_time, end_time):
                             file.write(line)
                     file.truncate()
 
+            # Sleep Application for 5 Seconds to Prevent Crashes from Infinite Loop
             time.sleep(5)
+
+            # Flush Standard Out from Continuous print() Calls
             sys.stdout.flush()
 
+
+
+    # Handle Invalid User Input
     except requests.exceptions.MissingSchema:
         print("ERROR: Invalid URL! No Schema Supplied!")
     except requests.exceptions.ConnectionError:
@@ -75,8 +112,14 @@ def block(os, redirect_site, websites, start_time, end_time):
     except ValueError:
         print("ERROR: Start Time and End Time: Hours must be between 0 and 23, Minutes must be between 0 and 59!")
 
+
+
+# main() Function
+# Handle User Input
+# Pass User Input to block() Function
 if __name__ == "__main__":
 
+    # Handle User Input
     argParser = argparse.ArgumentParser()
     argParser.add_argument("--os", required = True, help = "What's your Operating System: macOS, Linux, or Windows?")
     argParser.add_argument("--redirect_site", required = True, help = "Enter the URL you wish to Redirect to.")
@@ -85,4 +128,5 @@ if __name__ == "__main__":
     argParser.add_argument("--end_time", nargs='+', required = True, help = "TIme (Hour (Format: 0 - 23) and Minute (0 - 59)) to Terminate Website Blocking.")
     args = argParser.parse_args()
 
+    # Pass User Input to block() Function
     block(args.os, args.redirect_site, args.websites, args.start_time, args.end_time)
--- website_blocker/app_mac_linux_schedule.py
+++ website_blocker/app_mac_linux_schedule.py
@@ -1,16 +1,33 @@
+# Website Blocker Application
+
+# Blocks Websites from 8:00 AM until 4:00 PM
+# Can be Scheduled by macOS and Linux to Run Automatically on Boot Up 
+
+# Import Modules
 import time
 from datetime import datetime as dt
-
 import sys
 
+
+
+# Path to "Hosts" File (Enables Website Blocking)
 hosts_path = "/etc/hosts"
+
+# IP Address to Redirect User to when Accessing Target Website
 redirect = "127.0.0.1"
+
+# Target Website(s) to Block
 website_list = ["www.chess.com", "chess.com"]
 
+
+
+# Infinite Loop
 while True:
 
+    # If the Current Time is between 8:00 AM and 4:00 PM
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
 
+        # Add Target Website and IP Address to Redirect to in "Hosts" File if not Already there
         print("Work Hours...")
         with open(hosts_path, "r+") as file:
             content = file.read()
@@ -18,8 +35,10 @@ while True:
                 if website not in content:
                     file.write(redirect + " " + website + "\n")
 
+    # The Current Time is not between 8:00 AM and 4:00 PM
     else:
 
+        # Remove Target Website and IP Address to Redirect to from "Hosts" File if there
         print("Fun Hours...")
         with open(hosts_path, "r+") as file:
             content = file.readlines()
@@ -29,5 +48,8 @@ while True:
                     file.write(line)
             file.truncate()
 
+    # Sleep Application for 5 Seconds to Prevent Crashes from Infinite Loop
     time.sleep(5)
+
+    # Flush Standard Out from Continuous print() Calls
     sys.stdout.flush()
