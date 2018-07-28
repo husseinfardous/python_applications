0c5f4840bf810a503eef8485318fbbafc942f768
Hussein Fardous <hf2334@columbia.edu>
2018-07-28 11:33:01 -0400

    Website: Explained Code in Comments

--- website/app.py
+++ website/app.py
@@ -1,14 +1,32 @@
+# Website Application
+
+# Displays a Basic Website on "localhost:5000"
+# Gives Blueprints for Displaying a Website using Flask
+
+# Import Modules
 from flask import Flask, render_template
 
+
+
+# Configure Flask
+
+# Create an Instance of Flask Class for the Web Application
 app = Flask(__name__)
 
-@app.route('/')
+# Display Home Webpage
+@app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route('/about')
+# Display About Webpage
+@app.route("/about")
 def about():
     return render_template("about.html")
 
+
+
+# main() Function
+# Condition ensures that Web Application runs only if this module is run interactively
+# Web Application won't run if this module is imported into another module
 if __name__ == "__main__":
     app.run(debug = True)
